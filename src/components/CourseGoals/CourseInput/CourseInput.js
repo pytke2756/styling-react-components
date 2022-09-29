16b7b9bb93f848327377fcb21264f120a46590fd
Levente Ibrányi <levente.ibranyi@dpdgroup.com>
2022-09-29 13:35:28 +0200

    Dynamic styles with css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const FormControl = styled.div`
+/* const FormControl = styled.div`
   margin: 0.5rem 0;
 
   & label {
@@ -28,7 +28,7 @@ const FormControl = styled.div`
     background: #fad0ec;
     border-color: #8b005d;
   }
-`;
+`; */
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -51,7 +51,7 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles['invalid']}`}>
         {/* <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label> */}
         <label>Course Goal</label>
         {/* <input
@@ -63,7 +63,7 @@ const CourseInput = (props) => {
           onChange={goalInputChangeHandler}
         /> */}
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
